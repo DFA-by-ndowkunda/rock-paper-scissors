775798a18c9df5085317fbc914e696085d9f3347
ndowkunda <mn057@outlook.com>
2021-04-25 14:57:19 +0100

    update styling

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const Computer = require("../computer");
-const Player = require("../player");
 const Game = require("../game");
 const GameController = express.Router();
 
 GameController.post("/", (req, res) => {
+	req.app.locals.player = require("../player");
 	let game = new Game();
 	let computer = new Computer();
-	let player = new Player();
 	let computerChoice = computer.getWeapon();
 	let playerChoice = req.body.playerChoice;
 	game.addWeapon(playerChoice);
 	game.addWeapon(computerChoice);
 	res.render("pages/game", {
-		username: player.username,
+		username: req.app.locals.player.username,
 		playerWeapon: playerChoice,
 		computerWeapon: computerChoice,
 		result: game.checkWeapons(),
